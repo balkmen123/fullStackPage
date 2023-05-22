973da1e7661e965db58134bea39a4a8cce16654d
Nam cao <balkmen123@gmail.com>
2023-05-22 20:38:05 +0700

    end bai logic header#1

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -5,21 +5,37 @@ import AccountItem from '~/components/AccoutItem';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import styles from './Search.module.scss';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 const cx = classNames.bind(styles);
 
 function Search() {
+  const [searchValue, setSearchValue] = useState('');
   const [searchResult, setSearchResult] = useState([]);
+  const [showResult, setShowResult] = useState(true);
+
+  const inputRef = useRef();
+
   useEffect(() => {
     setTimeout(() => {
       setSearchResult([{ title: 'abc' }]);
     }, 0);
   }, []);
+
+  const handleHideResult = () => {
+    setShowResult(false);
+  };
+
+  const handleClear = () => {
+    setSearchValue('');
+    inputRef.current.focus();
+    setSearchResult([]);
+  };
+
   return (
     <HeadlessTippy
       interactive
-      visible={searchResult.length > 0}
+      visible={showResult && searchResult.length > 0}
       render={(attrs) => (
         <div className={cx('search-result')} tabIndex="-1" {...attrs}>
           <PopperWrapper>
@@ -31,13 +47,23 @@ function Search() {
           </PopperWrapper>
         </div>
       )}
+      onClickOutside={handleHideResult}
     >
       <div className={cx('search')}>
-        <input placeholder="Search accounts and videos" />
-        <button className={cx('clear')}>
-          <FontAwesomeIcon icon={faCircleXmark} />
-        </button>
-        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+        <input
+          ref={inputRef}
+          value={searchValue}
+          placeholder="Search accounts and videos"
+          onChange={(e) => setSearchValue(e.target.value)}
+          onFocus={() => setShowResult(true)}
+        />
+        {!!searchValue && (
+          <button className={cx('clear')} onClick={handleClear}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+        )}
+
+        {/* <FontAwesomeIcon className={cx('loading')} icon={faSpinner} /> */}
         <button className={cx('search-btn')}>
           <FontAwesomeIcon icon={faMagnifyingGlass} />
         </button>
